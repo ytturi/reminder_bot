f9225e6d05747dd3f7ea1904e39d6f2d297fdefa
ytturi <ytturi@protonmail.com>
2021-05-27 23:17:12 +0200

    Rename: template->reminder_bot

--- setup.py
+++ setup.py
@@ -1,23 +1,46 @@
 # -*- coding: utf-8 -*-
+
 # ------------------------------------------------------
+
 # Author: Ytturi
+
 # Author's e-mail: ytturi@protonmail.com
+
 # Version: 0.1
+
 # License: MIT
+
 # ------------------------------------------------------
+
 from setuptools import setup, find_packages
 
 
+
+
+
 setup(
-    name='python_pkg_template',
+
+    name='reminder_bot',
+
     version='0.1',
+
     packages=find_packages(),
+
     install_requires=[
+
         "click",
+
         "ConfigParser",
+
     ],
+
     entry_points='''
+
         [console_scripts]
-        do_smth=python_pkg_template.cli:do_smth
+
+        do_smth=reminder_bot.cli:do_smth
+
     ''',
+
 )
+
